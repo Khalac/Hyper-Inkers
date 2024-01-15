b2d41e94f0f5c3abe8d212341b392d17861260ac
Khalac <74862194+Khalac@users.noreply.github.com>
2024-01-15 15:09:44 +0700

    update Artist page

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from "react-dom/client";
 import MainPage from "./Page/MainPage/MainPage";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import SalePage from "./Page/SalePage/SalePage";
-import Minh from "./Page/Minh/Minh";
-<<<<<<< Updated upstream
-=======
+
 import Anet from "./Page/Anet/Anet";
 import Aaron from "./Page/Aaron/Aaron";
 import Nicole from "./Page/Nicole/Nicole";
 import Ben from "./Page/Ben/Ben";
 import AboutPage from "./Page/AboutPage/AboutPage";
->>>>>>> Stashed changes
+import Minh from "./Page/Minh/Minh";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -20,9 +18,6 @@ root.render(
       <Routes>
         <Route exac path="/" element={<SalePage />} />
         <Route exac path="/MainPage" element={<MainPage />} />
-<<<<<<< Updated upstream
-        <Route exac path="/Artist/Min" element={<Minh />} />
-=======
 
         <Route exac path="/AboutPage" element={<AboutPage />} />
 
@@ -31,7 +26,6 @@ root.render(
         <Route exac path="/Artist/Aaron" element={<Aaron />} />
         <Route exac path="/Artist/Nicole" element={<Nicole />} />
         <Route exac path="/Artist/Ben" element={<Ben />} />
->>>>>>> Stashed changes
       </Routes>
     </HashRouter>
   </React.StrictMode>
