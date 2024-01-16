4577e8dcf2f2d57226ef50903ab33787476265c8
Khalac <74862194+Khalac@users.noreply.github.com>
2024-01-16 07:39:39 +0700

    update

--- reactjs/src/Components/AboutPage/Artist_AboutPage/Artist_AboutPage.js
+++ reactjs/src/Components/AboutPage/Artist_AboutPage/Artist_AboutPage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Artist_AboutPage.scss";
 
+import BG from "../../assets/ImgMobile/Background/MainBG.png";
+
 function Artist_AboutPage() {
-  return <div className="Artist_AboutPage">
-     
-  </div>;
+  return (
+    <div className="Artist_AboutPage">
+      <img src={BG} alt="" className="Artist_AboutPage_BG" />
+    </div>
+  );
 }
 
 export default Artist_AboutPage;
